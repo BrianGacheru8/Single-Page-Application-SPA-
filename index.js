a61970723a6e0f7d42b5fc0cae0b9d2e2c932232
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-16 09:52:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded",()=>{
   fetch("http://localhost:3000/albums/")
-    .then(res=res.json()),
+    .then(res=>res.json());
     .then((data)=>{
-          data=albums,
-          albums.forEach((album){
-            spany=document.getElementById(spany),
-            tee=document.createElement("img"),
-            tee.src=album.coverimage,
+      data=albums;
+      console.log(albums);
+      albums.forEach((album){
+            spany=document.getElementById(spany);
+            tee=document.createElement("img");
+            tee.src=album.coverimage;
             spany.appendChild(tee)}})
     .catch(err=>alert(err))
                          })                                                  
