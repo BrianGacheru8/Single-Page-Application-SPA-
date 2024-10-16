9d5463ee25d53c1fc829bc177a1085f452afc315
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-16 10:08:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/albums/")
-    .then(res=>res.json())
-    .then((data)=>{;
-      data=albums;
+    .then(res => res.json())
+    .then((albums) => {
       console.log(albums);
-      albums.forEach((album){
-            spany=document.getElementById(spany);
-            tee=document.createElement("img");
-            tee.src=album.coverimage;
-            spany.appendChild(tee)}})
-    .catch(err=>alert(err))
-                         })                                                  
+      const spany = document.getElementById("spany");
+      albums.forEach((album) => {
+        const tee = document.createElement("img");
+        tee.src = album.coverImage;
+        spany.appendChild(tee);
+      });
+    })
+    .catch(err => {
+      console.error("Fetch error:", err);
+      alert("Failed to fetch album data. Please try again later.");
+    });
+});
+
                          
           
           
