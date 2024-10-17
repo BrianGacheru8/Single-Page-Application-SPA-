62985585b37bd2cfdb205c86162235b17bfe548d
BrianGacheru8 <briangacheru255@gmail.com>
2024-10-17 05:29:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,36 @@ document.addEventListener("DOMContentLoaded", () => {
       albums.forEach((album) => {
         const tee = document.createElement("img");
         tee.src = album.coverImage;
+        tee.addEventListener("click",(album){
+          dv=document.createElement("div")
+          dv.innerHTML=`
+            <p>${album.name} its rating is ${rating}</P>`,
+          tee.innerHTML(dv),
         spany.appendChild(tee);
-      });
+      }});
     })
     .catch(err => {
       console.error("Fetch error:", err);
       alert("Failed to fetch album data. Please try again later.");
     });
-document.getElementbyId("formy").addEventListener("submit",(){
+form1=document.getElementbyId("formy")
+form1.addEventListener("submit",(e){
+  e.preventDefault(),
+  fData= newFormData(form1),
+  data={
+    name=fData.name,
+    coverImage=fData.request,
+    rating=fData.rating,
+  };
   fetch("http://localhost:3000/albums/",{
-    method:"POST";
-    headers:{Content-type:"application/json"};
-        
-});
+    method:"POST",
+    headers:{Content-type:"application/json"},
+    body:{data.JSON.stringify(data)})
+  .then(res=>res.json())
+  .then(dt=>console.log(dt))
+  .catch(err=>alert(err))
+    }}    
+);
 
                          
           
